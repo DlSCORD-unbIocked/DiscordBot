fc066f27a505206ba605020ed6d27a3490768658
litblock <jonathanpeng2017@gmail.com>
2024-07-06 18:31:25 -0400

    adding options into commandinfo for easier command creation

--- ProjectManager/src/main/java/org/smartscholars/projectmanager/commands/CommandInfo.java
+++ ProjectManager/src/main/java/org/smartscholars/projectmanager/commands/CommandInfo.java
@@ -1,5 +1,6 @@
 package org.smartscholars.projectmanager.commands;
 
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,5 +12,5 @@ public @interface CommandInfo {
     String name();
     String description();
     Permission[] permissions() default {};
-
+    CommandOption[] options() default {};
 }
