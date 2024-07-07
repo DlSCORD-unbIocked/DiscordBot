aea1545b97a2e176b587ec6ce28beaa9196fb1a2
Andrew Ma <ama25@belmontschools.net>
2024-07-06 20:42:22 -0400

    added api, still broken

--- ProjectManager/src/main/java/org/smartscholars/projectmanager/commands/misc/ImageCommand.java
+++ ProjectManager/src/main/java/org/smartscholars/projectmanager/commands/misc/ImageCommand.java
@@ -9,11 +9,17 @@ import java.net.http.HttpResponse;
 import io.github.cdimascio.dotenv.Dotenv;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.nio.channels.Channel;
+import java.util.Base64;
+import java.util.Base64.Encoder;
 
 import javax.imageio.ImageIO;
+
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 
+import net.dv8tion.jda.api.utils.FileUpload;
 import org.smartscholars.projectmanager.commands.CommandInfo;
 import org.smartscholars.projectmanager.commands.CommandManager;
 import org.smartscholars.projectmanager.commands.ICommand;
@@ -39,7 +45,7 @@ public class ImageCommand implements ICommand {
         String url = event.getOption("image_url").getAsString();
         String effect = event.getOption("effect").getAsString();
         String apiUrl = "https://api.jeyy.xyz/v2/image/" + effect + "?url=" + url;
-
+        String apiUpload = "https://api.jeyy.xyz/v2/general/image_upload";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -47,28 +53,42 @@ public class ImageCommand implements ICommand {
                 .header("Authorization", "Bearer " + authToken)
                 .build();
 
+
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             // Process the response body here
+
+
+            HttpRequest post = new Http
+                    .uri(URI.create(url))
+                    .header("Authorization", "Bearer " + authToken)
+
+
             String responseBody = response.body();
 
+//            String imageBytes = Base64.getEncoder().encodeToString(responseBody.getBytes());
+//            byte[] imageBytes = responseBody.getBytes();
+//            InputStream is = new ByteArrayInputStream(imageBytes);
+//
+//            //input stream is file
+//            BufferedImage bufferedImage = ImageIO.read(is);
+//
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            ImageIO.write(bufferedImage, "png", baos); // Use appropriate format, e.g., "png"
+//            byte[] bytes = baos.toByteArray();
+//            EmbedBuilder embed = new EmbedBuilder();
 
-//            byte[] imageBytes = Base64.getDecoder().decode(responseBody);
-            byte[] imageBytes = responseBody.getBytes();
-            InputStream is = new ByteArrayInputStream(imageBytes);
-            BufferedImage bufferedImage = ImageIO.read(is);
-
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(bufferedImage, "png", baos); // Use appropriate format, e.g., "png"
-            byte[] bytes = baos.toByteArray();
-
-
-            event.reply("Command with options has been set up. URL: " + url + ", Effect: " + effect + ". Response: " ).addFiles().queue();
+            embed.setImage("attachment://image.png");
+            event.replyEmbeds(embed.build())
+                    .addFiles(FileUpload.fromData(imageBytes, "image.png"))
+                    .queue();
+//            event.replyFiles().queue();
+//            event.reply("Command with options has been set up. URL: " + url + ", Effect: " + effect + ". Response: " ).queue();
         } catch (IOException | InterruptedException e) {
             event.reply("An error occurred while fetching the URL.").queue();
         }
 
-        event.reply("Command with options has been set up " + url + effect).queue();
+//        event.reply("Command with options has been set up " + url + effect).queue();
 
     }
 }
