8139bdceb416a049c6608abbab11f51153aa2159
litblock <jonathanpeng2017@gmail.com>
2024-07-06 21:16:54 -0400

    still broken

--- ProjectManager/src/main/java/org/smartscholars/projectmanager/commands/misc/ImageCommand.java
+++ ProjectManager/src/main/java/org/smartscholars/projectmanager/commands/misc/ImageCommand.java
@@ -1,14 +1,30 @@
 package org.smartscholars.projectmanager.commands.misc;
+import java.io.*;
+import java.awt.*;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.channels.Channel;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+import javax.imageio.ImageIO;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+
+import net.dv8tion.jda.api.utils.FileUpload;
 import org.smartscholars.projectmanager.commands.CommandInfo;
 import org.smartscholars.projectmanager.commands.CommandManager;
-import org.smartscholars.projectmanager.commands.CommandOption;
 import org.smartscholars.projectmanager.commands.ICommand;
+import org.smartscholars.projectmanager.commands.CommandOption;
 
-import java.awt.*;
 
 @CommandInfo(
         name = "image",
@@ -22,6 +38,44 @@ public class ImageCommand implements ICommand {
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        event.reply("Command with options has been set up").queue();
+        // Define a command with options
+        Dotenv dotenv = Dotenv.load();
+        String authToken = dotenv.get("IMAGE_TOKEN");
+
+        String url = event.getOption("image_url").getAsString();
+        String effect = event.getOption("effect").getAsString();
+        String apiUrl = "https://api.jeyy.xyz/v2/image/" + effect + "?url=" + url;
+        String apiUpload = "https://api.jeyy.xyz/v2/general/image_upload";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl))
+                .header("Authorization", "Bearer " + authToken)
+                .build();
+
+
+        try {
+            HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
+            byte[] imageBytes = response.body();
+
+            if (imageBytes.length > 0) {
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.setTitle("Edited Image");
+                embed.setImage("attachment://image.png"); // This will be replaced by the actual image in the next step
+
+                event.replyEmbeds(embed.build())
+                        .addFiles(FileUpload.fromData(imageBytes, "image.png"))
+                        .queue();
+            }
+            else {
+                event.reply("No image was returned from the API.").setEphemeral(true).queue();
+            }
+        }
+        catch (IOException | InterruptedException e) {
+            event.reply("An error occurred while fetching the image.").setEphemeral(true).queue();
+        }
+
+//        event.reply("Command with options has been set up " + url + effect).queue();
+
     }
 }
